ccd3b3ed56303be4a142fb7f5a5da4a210a9f3c7
FarazMalik <rashumalik786@gmail.com>
2024-12-14 14:57:46 +0530

    changes

--- source/redux/Store.jsx
+++ source/redux/Store.jsx
@@ -2,12 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import FavouritePokemon_Reducer from './favouritePokemons';
 import { fakestoreApi } from './FakeStoreApi';
 
-
+function customMiddleware(store) {
+    return (next) => {
+        return (action) => {
+            next(action)
+        }
+    }
+}
 export const mystore = configureStore({
     reducer:{
         FavouritePokemon : FavouritePokemon_Reducer, 
-        [fakestoreApi.reducerPath] : fakestoreApi.reducer 
+        [fakestoreApi.reducerPath] : fakestoreApi.reducer
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(fakestoreApi.middleware),
+    getDefaultMiddleware().concat([fakestoreApi.middleware , customMiddleware]),
+    
 });
